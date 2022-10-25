f9920185ec87ff370d9aa3c55f99a66e7bfae7f6
LOKESH DEWDA <77046766+Lokeshdewada@users.noreply.github.com>
2022-10-25 13:27:30 +0530

    Added solution - LeetHub

---
+++ Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        //taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total count of testcases
+        int t = sc.nextInt();
+        
+        
+        
+        while(t-- > 0)
+        {
+            //taking total number of elements
+            int n = sc.nextInt();
+            
+            //taking size of subArrays 
+            int k = sc.nextInt();
+            
+            //Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+            
+            //adding all the elements to the array 
+            for(int i = 0; i <n; i++)
+            {
+                arr[i] =sc.nextInt();
+            }
+            
+            //Calling the method max_of_subarrays of class solve
+            //and storing the result in an ArrayList
+            ArrayList <Integer> res = new Solution().max_of_subarrays(arr, n, k);
+            
+            //printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print (res.get (i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find maximum of each subarray of size k.
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {
+        // Your code here
+        Deque<Integer> q=new ArrayDeque<>();
+        for(int i=0;i<k;i++){
+            while(q.size()>0 && q.peekLast()<arr[i]){
+                q.removeLast();
+            }
+            q.addLast(arr[i]);
+        }
+        ArrayList<Integer> ans =new ArrayList<>();
+        ans.add(q.peekFirst());
+        int x=0;
+        for(int i=k;i<n;i++){
+            if(q.peekFirst()==arr[x]){
+                q.removeFirst();
+            }
+            x++;
+            while(q.size()>0 && q.peekLast()<arr[i]){
+                q.removeLast();
+            }
+            q.addLast(arr[i]);
+
+            ans.add(q.peekFirst());
+        }
+        return ans;
+    }
+}
