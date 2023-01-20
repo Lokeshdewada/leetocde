ffa9c214f0176dcd74d414357ab5335677847655
LOKESH DEWDA <77046766+Lokeshdewada@users.noreply.github.com>
2023-01-20 08:29:18 +0530

    Added solution - LeetHub

--- Maximum Weight Node - GFG/maximum-weight-node.java
+++ Maximum Weight Node - GFG/maximum-weight-node.java
@@ -63,20 +63,17 @@ class GFG{
 
 class Solution{
     public int maxWeightCell(int N, int Edge[]){
-        int[] m=new int[N];
-        int ans=0;
-        for(int i=0;i<N;i++)
-            m[i]=0;
-        for(int j=0;j<N;j++)
-        {
-            if(Edge[j]>=0)
-                m[Edge[j]]=m[Edge[j]]+j;
-        }
-        for(int k=0;k<N;k++)
-        {
-            if(m[k]>m[ans])
-                ans=k;
-        }
-        return ans;
+       int [] map = new int [N];
+       for(int i=0;i<N;i++){
+           if(Edge[i] != -1){
+               map[Edge[i]] += i;
+           }
+       }
+       int res = 0;
+       for(int i=1;i<N;i++){
+           if(map[i]>map[res])
+           res=i;
+       }
+       return res;
     }
 }
