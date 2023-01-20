5d37715e1e13ba7e21c5fe840500dd8a5913dfc2
LOKESH DEWDA <77046766+Lokeshdewada@users.noreply.github.com>
2023-01-20 08:28:00 +0530

    Added solution - LeetHub

---
+++ Maximum Weight Node - GFG/maximum-weight-node.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class GFG{
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        Long nextLong(){
+            return Long.parseLong(next());
+        }
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int N = sc.nextInt();
+            int Edge[] = new int[N];
+            for(int i = 0; i < N; i++)
+                Edge[i] = sc.nextInt();
+            Solution ob = new Solution();
+            int ans = ob.maxWeightCell(N, Edge);
+            out.println(ans);            
+        }
+        out.flush();
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public int maxWeightCell(int N, int Edge[]){
+        int[] m=new int[N];
+        int ans=0;
+        for(int i=0;i<N;i++)
+            m[i]=0;
+        for(int j=0;j<N;j++)
+        {
+            if(Edge[j]>=0)
+                m[Edge[j]]=m[Edge[j]]+j;
+        }
+        for(int k=0;k<N;k++)
+        {
+            if(m[k]>m[ans])
+                ans=k;
+        }
+        return ans;
+    }
+}
