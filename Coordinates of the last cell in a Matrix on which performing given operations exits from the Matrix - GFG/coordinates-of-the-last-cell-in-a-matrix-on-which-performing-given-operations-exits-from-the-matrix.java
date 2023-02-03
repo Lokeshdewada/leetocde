b12cf19c85ca028ed30f26050754ec605e68b842
LOKESH DEWDA <77046766+Lokeshdewada@users.noreply.github.com>
2023-02-03 09:54:22 +0530

    Added solution - LeetHub

--- Coordinates of the last cell in a Matrix on which performing given operations exits from the Matrix - GFG/coordinates-of-the-last-cell-in-a-matrix-on-which-performing-given-operations-exits-from-the-matrix.java
+++ Coordinates of the last cell in a Matrix on which performing given operations exits from the Matrix - GFG/coordinates-of-the-last-cell-in-a-matrix-on-which-performing-given-operations-exits-from-the-matrix.java
@@ -43,44 +43,43 @@ class GFG
 
 
 //User function Template for Java
-
 class Solution{
     static int [] endPoints(int [][]arr, int m, int n){
         //code here
-        int i=0,j=0,inci=0,incj=1;
-        while(i<m && j<n && i>=0 && j>=0)
-        {
-            if(arr[i][j]==1)
-            {
-                arr[i][j]=0;
-                if(inci==-1)
-                {
-                    inci=0;
-                    incj=1;
-                }
-                else
-                if(incj==1)
-                {
-                    incj=0;
-                    inci=1;
-                }
-                else
-                if(inci==1)
-                {
-                    inci=0;
-                    incj=-1;
-                }
-                else
-                if(incj==-1)
-                {
-                    inci=-1;
-                    incj=0;
-                }
-            }
-            i+=inci;
-            j+=incj;
+        int i=0;int j=0; int curr_dir=1;int curr_i=0;int curr_j=0;
+
+        int dx[]={-1,0,1,0};
+
+        int dy[]={0,1,0,-1};
+
+        while(i>=0 && j>=0 && i<m && j<n){
+
+          curr_i=i;
+
+          curr_j=j;
+
+          if(arr[i][j]==0){
+
+             i+=dx[curr_dir];
+
+             j+=dy[curr_dir];
+
+          }else{
+
+               curr_dir=( curr_dir+1)%4;
+
+               arr[i][j]=0;
+
+                 i+=dx[curr_dir];
+
+                 j+=dy[curr_dir];
+
+
+
+          }
+
         }
-        int ans[]={(i+(-1*inci)),(j+(-1*incj))};
-        return ans;
+
+        return new int[] {curr_i,curr_j} ;
     }
 }
