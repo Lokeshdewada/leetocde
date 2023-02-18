cadc11962c6f5071a1f0f70373d1acab6dffffac
LOKESH DEWDA <77046766+Lokeshdewada@users.noreply.github.com>
2023-02-18 12:34:37 +0530

    Added solution - LeetHub

---
+++ Apple Sequences - GFG/apple-sequences.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input[] = read.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(input[0]);
+            int M = Integer.parseInt(input[1]);
+            String arr = read.readLine().trim();
+
+            Solution ob = new Solution();
+            out.println(ob.appleSequence(N, M, arr));
+        }
+        out.close();
+    }
+}
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution{
+    public static int appleSequence(int n, int m, String arr){
+        //code here
+        int count = 0;
+        int i = 0, j = 0;
+        int maxlen = Integer.MIN_VALUE;
+
+        while (j < n) {
+            char ch = arr.charAt(j);
+            if (ch == 'O') {
+                count++;
+            }
+            j++;
+
+            while (count > m) {
+                ch = arr.charAt(i);
+                if (ch == 'O') {
+                    count--;
+                }
+                i++;
+            }
+            maxlen = Math.max(maxlen, j-i);
+        }
+
+        return maxlen;
+    }
+}
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
