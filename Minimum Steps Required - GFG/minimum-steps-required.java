a00797f87db2389a8264ef9416b729cf664119e0
LOKESH DEWDA <77046766+Lokeshdewada@users.noreply.github.com>
2023-04-04 13:14:49 +0530

    Added solution - LeetHub

---
+++ Minimum Steps Required - GFG/minimum-steps-required.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG{
+	public static void main(String [] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		while(test-- > 0) {
+			String str = br.readLine();
+			Solution obj = new Solution();
+			System.out.println(obj.minSteps(str));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+	int minSteps(String str) {
+		int cnt=1;
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)!=str.charAt(i-1)){
+                cnt++;
+            }
+        }
+        return cnt/2 + 1;
+	}
+
+}
