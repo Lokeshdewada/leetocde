580e77b543cc5d48401801adea2f921bff825176
LOKESH DEWDA <77046766+Lokeshdewada@users.noreply.github.com>
2023-04-05 17:41:31 +0530

    Added solution - LeetHub

---
+++ Count Special Numbers - GFG/count-special-numbers.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        Long nextLong(){
+            return Long.parseLong(next());
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt(); // Inputting the testcases
+        PrintWriter out = new PrintWriter(System.out);
+        while (t-- > 0) {
+
+            int i = 0;
+            int N = sc.nextInt();
+
+            int arr[] = new int[(int)(N)];
+
+            for (i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Solution ob = new Solution();
+            out.println(ob.countSpecialNumbers(N, arr));
+        }
+        out.flush();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int countSpecialNumbers(int N, int arr[]) {
+        // Code here
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<N;i++){
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        }
+
+        int count = 0;
+
+        for(int i=0;i<N;i++){
+            if(hm.containsKey(arr[i])){
+                for(int j=0;j<N;j++){
+                    if(i!=j && arr[i]%arr[j]==0){
+                        count += hm.get(arr[i]);
+                        hm.remove(arr[i]);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
