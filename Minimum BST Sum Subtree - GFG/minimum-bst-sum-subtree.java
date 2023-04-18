f88aaf6f49de130bfe57e6c5b7e2c34f85557428
LOKESH DEWDA <77046766+Lokeshdewada@users.noreply.github.com>
2023-04-18 09:59:08 +0530

    Added solution - LeetHub

---
+++ Minimum BST Sum Subtree - GFG/minimum-bst-sum-subtree.java
@@ -0,0 +1,151 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
+    public static Node buildTree(String str) {
+        // Corner Case
+        if (str.length() == 0 || str.charAt(0) == 'N') return null;
+
+        // Creating array of Strings from input
+        // String after spliting by space
+        String ip[] = str.split(" ");
+
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+
+        // Push the root to the queue
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static Node inputTree(BufferedReader br) throws IOException {
+        return buildTree(br.readLine().trim());
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int target;
+            target = Integer.parseInt(br.readLine());
+
+            Node root = Node.inputTree(br);
+
+            Solution obj = new Solution();
+            int res = obj.minSubtreeSumBST(target, root);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    static int min=Integer.MAX_VALUE;
+    public static int minSubtreeSumBST(int target, Node root) {
+        min=Integer.MAX_VALUE;
+        helper(root,target);
+        return min==Integer.MAX_VALUE?-1:min;
+    }
+    static int[]helper(Node root,int target)
+    {
+        if(root==null)
+            return new int[]{0,0,0,0};
+        int[]left=helper(root.left,target);
+        int[]right=helper(root.right,target);
+        int data=root.data;
+        if((left[2]==0 || Math.max(left[1],left[0])<data) &&( right[2]==0 || Math.min(right[0],right[1])>data))
+        {
+            int sum=left[3]+right[3]+data;
+            if(sum==target)
+                min=Math.min(min,left[2]+right[2]+1);
+            if(left[2]==0)left[0]=data;
+            if(right[2]==0)right[1]=data;
+            return new int[]{left[0],right[1],left[2]+right[2]+1,sum};
+        }
+        return new int[]{Integer.MAX_VALUE,Integer.MIN_VALUE,-1,0};
+    }
+}
