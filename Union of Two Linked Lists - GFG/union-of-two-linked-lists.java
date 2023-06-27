d4e5e371d31ea36b20d2d002f6ac7c62d004cb4e
LOKESH DEWDA <77046766+Lokeshdewada@users.noreply.github.com>
2023-06-27 15:30:09 +0530

    Added solution - LeetHub

---
+++ Union of Two Linked Lists - GFG/union-of-two-linked-lists.java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+
+class GfG
+{
+    static Scanner sc = new Scanner(System.in);
+    
+    public static Node inputList(int size)
+    {
+        Node head, tail;
+        int val;
+        
+        val = sc.nextInt();
+        
+        head = tail = new Node(val);
+        
+        size--;
+        
+        while(size-->0)
+        {
+            val = sc.nextInt();
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+        
+        return head;
+    }
+    
+    public static void printList(Node n)
+    {
+        while(n!=null)
+        {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int n , m;
+            
+            n = sc.nextInt();
+            Node head1 = inputList(n);
+            
+            m = sc.nextInt();
+            Node head2 = inputList(m);
+            
+            Sol obj = new Sol();
+            
+            printList(obj.findUnion(head1, head2));
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+class Sol
+{
+public static Node findUnion(Node head1,Node head2)
+	{
+	    //Add your code here.
+	    TreeMap<Integer,Node> map = new TreeMap<>();
+	    while(head1 != null){
+	        map.putIfAbsent(head1.data , head1);
+	        head1 = head1.next;
+	    }
+	    while(head2 != null){
+	        map.putIfAbsent(head2.data , head2);
+	        head2 = head2.next;
+	    }
+	    Node head = new Node(-1);
+	    Node temp = head;
+	    for(int k : map.keySet()){
+	        temp.next = map.get(k);
+	        temp = temp.next;
+	        temp.next = null;
+	    }
+	    return head.next;
+	}
+}
